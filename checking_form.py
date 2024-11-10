f99646ff2afcf644c8f397cd488cde9c4ced8120
sribharath <sribharathq@gmail.com>
2024-11-10 12:09:53 +0530

    exception handling

--- checking_form.py
+++ checking_form.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+
 
 url = "http://127.0.0.1:5500/week1.html"
 
@@ -14,8 +16,13 @@ password=driver.find_element(By.ID, "password")
 username.send_keys("bharath")
 password.send_keys("12345")
 
-search_box = driver.find_element(By.ID, "loginBtn")
-search_box.send_keys(Keys.RETURN)
+
+try:
+    search_box = driver.find_element(By.ID, "loginBtnn")
+    search_box.send_keys(Keys.RETURN)
+except NoSuchElementException:
+    print("__no such id is occurs__")
+
 
 
 time.sleep(1000)
